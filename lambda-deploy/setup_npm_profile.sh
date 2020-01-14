e36a11cbe6da04e5551bf1a96062d510a947db0b
Hugh Nimmo-Smith <hugh@nimmo-smith.com>
2020-01-14 17:03:42 +0000

    Auto handle NPM_URL format

--- lambda-deploy/setup_npm_profile.sh
+++ lambda-deploy/setup_npm_profile.sh
@@ -5,8 +5,9 @@
 if [[ "$NPM_URL" != "" ]]; then
   if [[ "$NPM_TOKEN" != "" ]]; then
     npm set registry ${NPM_URL} --always-auth
-    npm set '//${NPM_URL}:_authToken' '${NPM_TOKEN}'
+    AUTH_URL=`echo ${NPM_URL} | sed "s,.*://,//,"`
+    npm set '${AUTH_URL}:_authToken' '${NPM_TOKEN}'
   else
-    npm set registry 'https://${NPM_URL}'
+    npm set registry ${NPM_URL}
   fi;
 fi
