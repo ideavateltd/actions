b2cc4f4ff5459f9755726afa3aaf2f21a24e8516
Hugh Nimmo-Smith <hugh@nimmo-smith.com>
2020-01-14 17:13:57 +0000

    Fix env substitution

--- lambda-deploy/setup_npm_profile.sh
+++ lambda-deploy/setup_npm_profile.sh
@@ -6,7 +6,7 @@ if [[ "$NPM_URL" != "" ]]; then
   if [[ "$NPM_TOKEN" != "" ]]; then
     npm set registry ${NPM_URL} --always-auth
     AUTH_URL=`echo ${NPM_URL} | sed "s,.*://,//,"`
-    npm set '${AUTH_URL}:_authToken' '${NPM_TOKEN}'
+    npm set "${AUTH_URL}:_authToken" "${NPM_TOKEN}"
   else
     npm set registry ${NPM_URL}
   fi;
