600fe04054f43a36c8e7c2ab13b3491c2cd91bb4
Hugh Nimmo-Smith <hugh@nimmo-smith.com>
2021-01-30 21:16:21 +0000

    Revert "fix: alpine doesn't come with curl pre installed"
    This reverts commit b622cf1d8b7b8cb8f55bdd5146ec53d1e4ef9fc1.

--- lambda-deploy/deploy_to_lambda.sh
+++ lambda-deploy/deploy_to_lambda.sh
@@ -21,14 +21,14 @@ elif [[ "$MVN_GROUPID" != "" ]]; then
   if [[ "${VERSION}" == "latest" || "${VERSION}" == "release" ]]; then
     META_URL="${MVN_REPO}/${GROUP_PATH}/${MVN_ARTIFACTID}/maven-metadata.xml"
     echo "Fetching metadata from ${META_URL} to resolve ${VERSION}"
-    RESOLVED_VERSION=`wget --user="${MVN_USERNAME}" --password="${MVN_PASSWORD}" "${META_URL}" -O - | tr '\n' ' ' | sed -E "s,.*<${VERSION}>(.*)</${VERSION}>.*,\1,"`
+    RESOLVED_VERSION=`curl -\# -f -u "${MVN_USERNAME}:${MVN_PASSWORD}" "${META_URL}" | tr '\n' ' ' | sed -E "s,.*<${VERSION}>(.*)</${VERSION}>.*,\1,"`
     echo "${VERSION} version was ${RESOLVED_VERSION}"
     VERSION=${RESOLVED_VERSION}
   fi
 
   ARTIFACT_URL="${MVN_REPO}/${GROUP_PATH}/${MVN_ARTIFACTID}/${VERSION}/${MVN_ARTIFACTID}-${VERSION}-lambda.zip"
   echo "Fetching ${ARTIFACT_URL}"
-  wget --user="${MVN_USERNAME}" --password="${MVN_PASSWORD}" -O package.zip "${ARTIFACT_URL}"
+  curl -# -f -u "${MVN_USERNAME}:${MVN_PASSWORD}" -o package.zip "${ARTIFACT_URL}"
   unzip package.zip serverless.yml
 fi
 
