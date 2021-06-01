783e9ca763086d9ed265512a6b47ec2a6cd2c9b0
Hugh Nimmo-Smith <hugh@nimmo-smith.com>
2021-06-01 18:47:17 +0100

    fix: ensure always-auth=true is set on npm

--- lambda-deploy/setup_npm_profile.sh
+++ lambda-deploy/setup_npm_profile.sh
@@ -8,11 +8,12 @@ if [[ "$NPM_URL" != "" ]]; then
     # Create aws codeartifact get-authorization-token command to issue token
     npmLogin=`echo "$NPM_URL" | sed -E 's,^https://([a-z]+)-([0-9]+)\.d\.codeartifact\.([a-z][a-z]-[a-z]+-[0-9]+)\.amazonaws.com/npm/([a-z]+)/$,aws codeartifact get-authorization-token --domain \1 --domain-owner \2 --region \3 --query authorizationToken --output text,'`
     echo "Calling ${npmLogin}"
-    NPM_TOKEN=`${npmLogin} | tr -d '[:space:]'`
+    NPM_TOKEN=`${npmLogin}`
   fi
   if [[ "$NPM_TOKEN" != "" ]]; then
-    npm set registry ${NPM_URL} --always-auth
+    npm set registry ${NPM_URL}
     AUTH_URL=`echo ${NPM_URL} | sed "s,.*://,//,"`
+    npm set "${AUTH_URL}:always-auth=true"
     npm set "${AUTH_URL}:_authToken" "${NPM_TOKEN}"
   else
     npm set registry ${NPM_URL}
