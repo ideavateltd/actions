52d414e0a1009b418aa6f213919e9926782f5f75
Hugh Nimmo-Smith <hugh@nimmo-smith.com>
2021-06-01 17:19:48 +0100

    feat: support login to aws codeartifact npm registry

--- lambda-deploy/setup_npm_profile.sh
+++ lambda-deploy/setup_npm_profile.sh
@@ -3,6 +3,13 @@
 # Setup NPM environment
 
 if [[ "$NPM_URL" != "" ]]; then
+  # AWS CodeArtifact auth
+  if [[ "$NPM_TOKEN" == "" && "$NPM_URL" =~ codeartifact\..*amazonaws\.com ]]; then
+    # Create aws codeartifact get-authorization-token command to issue token
+    npmLogin=`echo "$NPM_URL" | sed -E 's,^https://([a-z]+)-([0-9]+)\.d\.codeartifact\.([a-z][a-z]-[a-z]+-[0-9]+)\.amazonaws.com/npm/([a-z]+)/$,aws codeartifact get-authorization-token --domain \1 --domain-owner \2 --region \3 --query authorizationToken --output text,'`
+    echo "Calling ${npmLogin}"
+    NPM_TOKEN=`${npmLogin} | tr -d '[:space:]'`
+  fi
   if [[ "$NPM_TOKEN" != "" ]]; then
     npm set registry ${NPM_URL} --always-auth
     AUTH_URL=`echo ${NPM_URL} | sed "s,.*://,//,"`
